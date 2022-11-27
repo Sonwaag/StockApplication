a4452540d64e02b247255d084d32f1991d818935
luddebassen <luddebassen213@gmail.com>
2022-11-27 16:26:21 +0100

    Chart.js download and chart for company

--- StockApplication/ClientApp/src/app/company/company.component.ts
+++ StockApplication/ClientApp/src/app/company/company.component.ts
@@ -1,7 +1,8 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
 import { clientCompany } from '../TsClasses';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
     templateUrl: 'company.component.html'
@@ -9,6 +10,8 @@ import { Router } from '@angular/router';
 
 export class CompanyComponent {
     public company: clientCompany;
+    public chart: any;
+    @ViewChild('lineChart', { static: true }) private chartRef;
 
     ngOnInit() {
         this.getCurrentCompany();
@@ -22,9 +25,28 @@ export class CompanyComponent {
         this.http.get<clientCompany>("api/Stock/company/current")
             .subscribe(company => {
                 this.company = company;
+                this.createChart(company);
             },
             error => console.log(error)
         );
+    }
+
+    createChart(company: clientCompany) {
+        const parse = JSON.parse(company.values);
+        this.chart = new Chart(this.chartRef.nativeElement, { //new chart with context(ctx) and assigning data + designing, companyChart is global variable because we need to access it in our ajax call
+            type: 'line',
+            data: {
+                labels: ["10 mins ago", "9 mins ago", "8 mins ago", "7 mins ago", "6 mins ago", "5 mins ago", "4 mins ago", "3 mins ago", "2 mins ago", "1 min ago"],
+                datasets: [{
+                    label: company.name,
+                    data: parse,
+                    fill: false,
+                    borderColor: 'rgb(75,192,192)',
+                    pointHitRadius: 100,
+                    pointRadius: 5
+                }]
+            }
+        }); 
     }
 
 }
